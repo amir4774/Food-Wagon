4f4779ac8016131ada58a087fa5c364fcffb0cf3
amir4774 <amirabbassi1382@gmail.com>
2023-12-07 22:49:30 +0330

    images-HowWork in js

--- src/components/HowWork.jsx
+++ src/components/HowWork.jsx
@@ -1,7 +1,14 @@
 import { StyledHowWork } from "../style/HowWork.styled";
 import HowWorkData from "../utils/HowWork";
+import Map_Marker from "../assets/Map_Marker.png";
+import order from "../assets/order.png";
+import pay from "../assets/pay.png";
+import enjoy from "../assets/enjoy.png";
 
 const HowWork = () => {
+
+  const images = [Map_Marker, order, pay, enjoy];
+
   return (
     <StyledHowWork>
       <div className="container">
@@ -10,7 +17,7 @@ const HowWork = () => {
         <div className="icons">
           {HowWorkData.map((item) => (
             <div key={item.id}>
-              <img src={item.src} alt="" />
+              <img src={images[item.id]} alt="" />
               <h4 className="how-title">{item.title}</h4>
               <p className="how-caption">{item.caption}</p>
             </div>
