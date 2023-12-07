ca0595a5e86669ece198bb964a452fee01467838
amir4774 <amirabbassi1382@gmail.com>
2023-12-07 22:42:10 +0330

    images-Details in js

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,8 +1,13 @@
 import detailsData from "../utils/Details.json";
 import DetailCard from "./DetailCard";
 import { Container } from "../style/Container";
+import detail_1 from "../assets/detail_1.png";
+import detail_2 from "../assets/detail_2.png";
+import detail_3 from "../assets/detail_3.png";
 
 const Details = () => {
+  const images = [detail_1, detail_2, detail_3];
+
   return (
     <section>
       <Container>
@@ -10,7 +15,7 @@ const Details = () => {
           <DetailCard
             key={detail.id}
             id={detail.id}
-            src={detail.src}
+            src={images[detail.id - 1]}
             title={detail.title}
             yellow_title={detail.yellow_title}
             description={detail.description}
